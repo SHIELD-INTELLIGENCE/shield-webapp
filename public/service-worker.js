aa8ec1f9483322bb852f7ac870be7bed37dc392b
reyanshrajmishra <reyanshrajmishra@gmail.com>
2025-06-21 13:18:23 +0530

    fixed production build

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,14 +2,24 @@ self.addEventListener('install', (e) => {
   console.log('[ServiceWorker] Install');
   e.waitUntil(
     caches.open('shield-pwa-cache').then((cache) => {
+      // Cache only your app shell and any static files
       return cache.addAll(['/', '/index.html']);
     })
   );
 });
 
 self.addEventListener('fetch', (e) => {
-  console.log('[ServiceWorker] Fetch', e.request.url);
+  const url = new URL(e.request.url);
+
+  // Bypass the service worker for assets
+  if (url.pathname.startsWith('/assets/')) {
+    return; // Don't respond, let the network take it
+  }
+
+  // Otherwise serve from cache or fetch
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
   );
 });
