d8c378613afe319042b1c931a2570f1c0ad1eeb5
reyanshrajmishra <reyanshrajmishra@gmail.com>
2025-06-21 12:59:11 +0530

    fix: css error and production build

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/', // Make sure assets use absolute paths
   plugins: [react()],
   server: {
     host: true // Allow mobile access on LAN
+  },
+  build: {
+    outDir: 'dist', // Output matches what Netlify expects
   }
 });
