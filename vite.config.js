f91134286b5d3362c5f854ec2e6305ee89c62b2e
reyanshrajmishra <reyanshrajmishra8@gmail.com>
2025-07-06 18:09:21 +0530

    IDK

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // <-- relative paths
   plugins: [react()],
   build: {
     outDir: 'dist',
-  }
+  },
 });
